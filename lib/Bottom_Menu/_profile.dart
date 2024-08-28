ce566b5c61a26a7e51714edba47ba918377f3e0b
Rey Yuuki <irfan31331@gmail.com>
2024-08-28 16:39:14 +0530

    Create _profile.dart

---
+++ lib/Bottom_Menu/_profile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("This page content seeing soon", style: TextStyle(fontSize: 20),)),
+    );
+  }
+}
