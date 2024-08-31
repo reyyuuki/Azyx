bf49361bd8e9ccecb8bf17efb4f78ba5cdb181f7
Rey Yuuki <irfan31331@gmail.com>
2024-08-31 22:28:23 +0530

    fixing not updating border

--- lib/components/_colorball.dart
+++ lib/components/_colorball.dart
@@ -1,5 +1,6 @@
 import 'package:daizy_tv/Provider/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
 
 class ColorBall extends StatelessWidget {
@@ -8,16 +9,18 @@ class ColorBall extends StatelessWidget {
   String? name;
   
    ColorBall({super.key,this.color,required this.name});
+   
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
-    bool isBorder = themeProvider.seedColor == color ? true : false;
+    bool isBorder = themeProvider.seedColor!.value == color.value ? true : false;
     return Column(
       children: [
         GestureDetector(
           onTap: () { 
             themeProvider.updateSeedColor(color!);
+            Hive.box('mybox').put('SeedColor', color);
           },
           child: Container(
             decoration: BoxDecoration(border: Border.all(width: 2, color: isBorder ? color!: Colors.transparent), borderRadius: BorderRadius.circular(20)),
