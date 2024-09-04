20c7916c3c9ef27572a244bbcad3eb35f51ece60
Rey Yuuki <irfan31331@gmail.com>
2024-09-04 18:38:43 +0530

    making gridlist for manga search

--- lib/components/Anime/_gridlist.dart
+++ lib/components/Anime/_gridlist.dart
@@ -12,64 +12,70 @@ class GridList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      physics:
-          const NeverScrollableScrollPhysics(), 
-      shrinkWrap: true, 
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, 
-        crossAxisSpacing: 20.0, 
-        childAspectRatio:
-            0.5, 
-      ),
-      itemCount: data!.length,
-      itemBuilder: (context, index) {
-        final item = data![index];
-        return GestureDetector(
-          onTap: () => Navigator.pushNamed(context, '/detailspage',
-          arguments: {"id": item['id'], "image": item['poster']}
-          ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 250,
-                width: 230,
-                child: Hero(
-                  tag: item['id'],
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: CachedNetworkImage(
-                      imageUrl: item['poster'],
-                      fit: BoxFit.cover,
-                      progressIndicatorBuilder:
-                          (context, url, downloadProgress) =>
-                              Center(
-                                child: CircularProgressIndicator(
-                                    value: downloadProgress.progress),
-                              ),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
+    if(data == null){
+      return const SizedBox.shrink();
+    }
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: GridView.builder( 
+        shrinkWrap: true, 
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, 
+          crossAxisSpacing: 20.0, 
+          childAspectRatio:
+              0.7, 
+        ),
+        itemCount: data!.length,
+        itemBuilder: (context, index) {
+          final item = data![index];
+          final tagg = item['id'] + "List";
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, '/detailspage',
+            arguments: {"id": item['id'], "image": item['poster'], "tagg": tagg}
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 170,
+                  width: 120,
+                  child: Hero(
+                    tag: tagg,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: CachedNetworkImage(
+                        imageUrl: item['poster'],
+                        fit: BoxFit.cover,
+                        progressIndicatorBuilder:
+                            (context, url, downloadProgress) =>
+                                Center(
+                                  child: CircularProgressIndicator(
+                                      value: downloadProgress.progress),
+                                ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Expanded(
-                child: TextScroll(
-                  item['name'],
-                  mode: TextScrollMode.bouncing,
-                  velocity: const Velocity(pixelsPerSecond: Offset(40, 0)),
-                  delayBefore: const Duration(milliseconds: 500),
-                  pauseBetween: const Duration(milliseconds: 1000),
-                  textAlign: TextAlign.center,
-                  selectable: true,
-                  style: const TextStyle(fontSize: 18),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 120,
+                  child: TextScroll(
+                    item['name'],
+                    mode: TextScrollMode.bouncing,
+                    velocity: const Velocity(pixelsPerSecond: Offset(40, 0)),
+                    delayBefore: const Duration(milliseconds: 500),
+                    pauseBetween: const Duration(milliseconds: 1000),
+                    textAlign: TextAlign.center,
+                    selectable: true,
+                    style: const TextStyle(fontSize: 18),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
