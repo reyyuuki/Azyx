c7184d19239830acab08da9de9f039f66fd07eaa
Rey Yuuki <irfan31331@gmail.com>
2024-09-04 18:43:06 +0530

    making awesome listView for anime search

---
+++ lib/pages/Anime/_search_list.dart
@@ -0,0 +1,134 @@
+import 'dart:ui';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class SearchList extends StatelessWidget {
+  dynamic data;
+  SearchList({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          final item = data[index];
+          final title = item['name'];
+          final episodes = item['episodes']['sub'].toString();
+          final image = item['poster'];
+          final id = item['id'];
+          final tagg = id + "List";
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(context, "/detailspage",
+                  arguments: {"id": id, "image": image, "tagg": tagg});
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: LinearGradient(colors: [
+                          Colors.transparent,
+                          Theme.of(context).colorScheme.primary
+                        ], begin: Alignment.center, end: Alignment.bottomCenter)),
+                    width: MediaQuery.of(context).size.width,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: ImageFiltered(
+                        imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                        child: CachedNetworkImage(
+                          imageUrl: image,
+                          fit: BoxFit.cover,
+                          progressIndicatorBuilder:
+                              (context, url, downloadProgress) => Center(
+                            child: CircularProgressIndicator(
+                                value: downloadProgress.progress),
+                          ),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.transparent,
+                              Theme.of(context).colorScheme.primary,
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ),
+                  Positioned(
+                      top: 0,
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 15),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              height: 170,
+                              width: 120,
+                              child: Hero(
+                                tag: tagg,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(20),
+                                  child: CachedNetworkImage(
+                                    imageUrl: image,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            SizedBox(
+                                width: 150,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      title.length > 40
+                                          ? title.substring(0, 37) + "..."
+                                          : title,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 16),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      'Episodes  $episodes',
+                                    )
+                                  ],
+                                )),
+                          ],
+                        ),
+                      ))
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
