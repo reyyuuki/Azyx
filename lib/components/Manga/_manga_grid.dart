7216abc80d986d2a57c874097b87217cf4dd9625
Rey Yuuki <irfan31331@gmail.com>
2024-09-04 18:41:28 +0530

    making gridlist for manga search

---
+++ lib/components/Manga/_manga_grid.dart
@@ -0,0 +1,81 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:text_scroll/text_scroll.dart';
+
+class MangaGrid extends StatelessWidget {
+  const MangaGrid({
+    super.key,
+    required this.data,
+  });
+
+  final dynamic data;
+
+  @override
+  Widget build(BuildContext context) {
+    if(data == null){
+      return const SizedBox.shrink();
+    }
+    return Container(
+      color: Theme.of(context).colorScheme.surface,
+      child: GridView.builder( 
+        shrinkWrap: true, 
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, 
+          crossAxisSpacing: 20.0, 
+          childAspectRatio:
+              0.7, 
+        ),
+        itemCount: data!.length,
+        itemBuilder: (context, index) {
+          final item = data![index];
+          final tagg = item['id'] + "List";
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, '/mangaDetail',
+            arguments: {"id": item['id'], "image": item['image'], "tagg": tagg}
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 170,
+                  width: 120,
+                  child: Hero(
+                    tag: tagg,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: CachedNetworkImage(
+                        imageUrl: item['image'],
+                        fit: BoxFit.cover,
+                        progressIndicatorBuilder:
+                            (context, url, downloadProgress) =>
+                                Center(
+                                  child: CircularProgressIndicator(
+                                      value: downloadProgress.progress),
+                                ),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: 120,
+                  child: TextScroll(
+                    item['title'],
+                    mode: TextScrollMode.bouncing,
+                    velocity: const Velocity(pixelsPerSecond: Offset(40, 0)),
+                    delayBefore: const Duration(milliseconds: 500),
+                    pauseBetween: const Duration(milliseconds: 1000),
+                    textAlign: TextAlign.center,
+                    selectable: true,
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
