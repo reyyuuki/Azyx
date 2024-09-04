610c2901989680348940671f49c7bab4ac5f0326
Rey Yuuki <irfan31331@gmail.com>
2024-09-04 13:46:29 +0530

    adding new list type and styles etc..

--- lib/pages/Anime/searchAnime.dart
+++ lib/pages/Anime/searchAnime.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
+import 'dart:ui';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:text_scroll/text_scroll.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SearchAnime extends StatefulWidget {
   String name;
   SearchAnime({super.key, required this.name});
-
-
 
   @override
   State<SearchAnime> createState() => _SearchpageState();
@@ -17,8 +16,7 @@ class SearchAnime extends StatefulWidget {
 class _SearchpageState extends State<SearchAnime> {
   dynamic data;
 
-
-TextEditingController? _controller;
+  TextEditingController? _controller;
 
   @override
   void initState() {
@@ -27,11 +25,11 @@ TextEditingController? _controller;
     fetchdata();
   }
 
-@override
-void dispose() {
-  _controller?.dispose();
-  super.dispose();
-}
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
 
   Future<void> fetchdata() async {
     String url =
@@ -64,101 +62,167 @@ void dispose() {
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
-        toolbarHeight: 80, 
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10),
-          child: TextField(
-            controller: _controller,
-            onSubmitted: (value) {
-              handleSearch(value);
-            },
-            decoration: InputDecoration(
-              prefixIcon: const Icon(Icons.search),
-              hintText: 'Search Anime',
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide:
-                    BorderSide(color: Theme.of(context).colorScheme.primary),
+      body: Column(
+        children: [
+          const SizedBox(height: 45),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back_ios,
+                  size: 30,
+                ),
               ),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: BorderSide.none,
+              SizedBox(
+                height: 55,
+                width: 280,
+                child: TextField(
+                  controller: _controller,
+                  onSubmitted: (value) {
+                    handleSearch(value);
+                  },
+                  decoration: InputDecoration(
+                    prefixIcon: const Icon(Icons.search),
+                    hintText: 'Search Anime',
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor:
+                        Theme.of(context).colorScheme.surfaceContainerHighest,
+                    filled: true,
+                  ),
+                ),
               ),
-              fillColor: Theme.of(context).colorScheme.surfaceContainerHighest,
-              filled: true,
+            ],
+          ),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Search Results",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: Row(
+                    children: [
+                      Icon(Iconsax.menu_15),
+                      SizedBox(width: 10),
+                      Icon(Iconsax.grid_25),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
-        ),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(Icons.arrow_back_ios),
-        ),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(10.0),
-        children: [
-          GridView.builder(
-            physics:
-                const NeverScrollableScrollPhysics(), 
-            shrinkWrap: true, 
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, 
-              crossAxisSpacing: 20.0, 
-              childAspectRatio:
-                  0.5, 
-            ),
-            itemCount: data!.length,
-            itemBuilder: (context, index) {
-              final item = data![index];
-              return GestureDetector(
-                onTap: () => Navigator.pushNamed(context, '/detailspage',
-                arguments: {"id": item['id'], "image": item['poster']}
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 250,
-                      width: 230,
-                      child: Hero(
-                        tag: item['id'],
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final item = data[index];
+                final title = item['name'];
+                final episodes = item['episodes']['sub'].toString();
+                final image = item['poster'];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            gradient: LinearGradient(
+                                colors: [
+                                  Colors.transparent,
+                                  Theme.of(context).colorScheme.primary
+                                ],
+                                begin: Alignment.center,
+                                end: Alignment.bottomCenter)),
+                        width: MediaQuery.of(context).size.width,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(20),
-                          child: CachedNetworkImage(
-                            imageUrl: item['poster'],
-                            fit: BoxFit.cover,
-                            progressIndicatorBuilder:
-                                (context, url, downloadProgress) =>
-                                    Center(
-                                      child: CircularProgressIndicator(
-                                          value: downloadProgress.progress),
-                                    ),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
+                          child: ImageFiltered(
+                            imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                            child: CachedNetworkImage(
+                              imageUrl: image,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    Expanded(
-                      child: TextScroll(
-                        item['name'],
-                        mode: TextScrollMode.bouncing,
-                        velocity: const Velocity(pixelsPerSecond: Offset(40, 0)),
-                        delayBefore: const Duration(milliseconds: 500),
-                        pauseBetween: const Duration(milliseconds: 1000),
-                        textAlign: TextAlign.center,
-                        selectable: true,
-                        style: const TextStyle(fontSize: 18),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.transparent,
+                                  Theme.of(context).colorScheme.primary,
+                                ],
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                              ),
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              );
-            },
+                      Positioned(
+                          top: 0,
+                          width: MediaQuery.of(context).size.width,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 15),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  height: 170,
+                                  width: 120,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(20),
+                                    child: CachedNetworkImage(
+                                      imageUrl: image,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 20,),
+                                SizedBox(
+                                  width:150,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(title.length > 40 ? title.substring(0,37) + "..." : title , style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),),
+                                    const SizedBox(height: 5,),
+                                    Text('Episodes  ' + item['episodes']['sub'].toString(),style: TextStyle( color: Theme.of(context).colorScheme.onSecondaryFixed),)
+                                  ],
+                                )),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
