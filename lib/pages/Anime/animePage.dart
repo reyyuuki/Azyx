0d4088b137b9f8ee6ab035f4aa22f9952aaf0af3
Rey Yuuki <irfan31331@gmail.com>
2024-09-10 16:52:35 +0530

    adding consumet data

--- lib/pages/Anime/animePage.dart
+++ lib/pages/Anime/animePage.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'package:daizy_tv/_anime_api.dart';
 import 'package:daizy_tv/backupData/anime.dart';
+import 'package:daizy_tv/backupData/anime_consumet_data.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:daizy_tv/components/Anime/carousel.dart';
 import 'package:daizy_tv/components/Header.dart';
@@ -19,7 +22,9 @@ class _HomepageState extends State<Animepage> {
   dynamic trendingAnime;
   dynamic latestEpisodesAnime;
   dynamic topUpComingAnime;
-
+  dynamic baseData;
+  var box = Hive.box("app-data");
+  bool isConsumet = true;
 
   @override
   void initState() {
@@ -29,42 +34,54 @@ class _HomepageState extends State<Animepage> {
   }
 
   void backUpData() {
-    spotlightAnime = animeData['spotlightAnimes'];
-    trendingAnime = animeData['trendingAnimes'];
-    latestEpisodesAnime = animeData['latestEpisodeAnimes'];
-    topUpComingAnime = animeData['topUpcomingAnimes'];
-  }
-
-  Future<void> fetchData() async {
-    try {
-      final response = await http
-          .get(Uri.parse("https://goodproxy.goodproxy.workers.dev/fetch?url=https://aniwatch-ryan.vercel.app/anime/home"));
-      if (response.statusCode == 200) {
-        final jsonData = jsonDecode(response.body);
-        setState(() {
-          spotlightAnime = jsonData['spotlightAnimes'];
-          trendingAnime = jsonData['trendingAnimes'];
-          latestEpisodesAnime = jsonData['latestEpisodeAnimes'];
-          topUpComingAnime = jsonData['topUpcomingAnimes'];
-        });
-      } else {
-        throw Exception("Failed to load data: ${response.statusCode}");
-      }
-    } catch (error) {
-      log('$error');
+    if (isConsumet) {
+      setState(() {
+        spotlightAnime = extractData( consumet_spotlightAnimes["results"]);
+        trendingAnime = extractData(consumet_trendingAnimes["results"]);
+        latestEpisodesAnime = extractData(consumet_latestEpisodeAnimes["results"]);
+        topUpComingAnime = extractData(consumet_topUpcomingAnimes["results"]);
+      });
+    } else {
+      setState(() {
+        spotlightAnime = animeData['spotlightAnimes'];
+        trendingAnime = animeData['trendingAnimes'];
+        latestEpisodesAnime = animeData['latestEpisodeAnimes'];
+        topUpComingAnime = animeData['topUpcomingAnimes'];
+      });
     }
   }
 
+
+  Future<void> fetchData() async{
+    if (isConsumet) {
+     baseData = await consumetHomePageData();
+    } else {
+    baseData = await fetchHomePageData();
+    }
+  }
+
+
+  void updateData(){
+    setState(() {
+      spotlightAnime = extractData(baseData["spotlightAnimes"]);
+      trendingAnime = extractData(baseData["trendingAnimes"]);
+      latestEpisodesAnime = extractData(baseData["latestEpisodesAnimes"]);
+      topUpComingAnime = extractData(baseData["topUpComingAnimes"]);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView(
           children: [
-            const Header(name: "Anime",),
+            const Header(
+              name: "Anime",
+            ),
             const SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -95,13 +112,27 @@ class _HomepageState extends State<Animepage> {
               ),
             ),
             const SizedBox(height: 30.0),
-            Carousel(animeData: trendingAnime,),
+            Carousel(
+              animeData: trendingAnime,
+            ),
             const SizedBox(height: 30.0),
-            ReusableList(name: 'Popular', data: spotlightAnime, taggName: "Carousale1",),
+            ReusableList(
+              name: 'Popular',
+              data: spotlightAnime,
+              taggName: "Carousale1",
+            ),
             const SizedBox(height: 10),
-            ReusableList(name: 'Latest Episodes', data: latestEpisodesAnime, taggName: "Carousale2",),
+            ReusableList(
+              name: 'Latest Episodes',
+              data: latestEpisodesAnime,
+              taggName: "Carousale2",
+            ),
             const SizedBox(height: 10),
-            ReusableList(name: 'Top Upcoming', data: topUpComingAnime, taggName: "Carousale3",),
+            ReusableList(
+              name: 'Top Upcoming',
+              data: topUpComingAnime,
+              taggName: "Carousale3",
+            ),
             const SizedBox(height: 10),
           ],
         ),
