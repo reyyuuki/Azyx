a8c089e9b7ee084fb5369affd8baf95a5fb27350
Rey Yuuki <irfan31331@gmail.com>
2024-10-01 16:46:34 +0530

    - Changing fonts and adjusting styles

--- lib/components/Anime/carousel.dart
+++ lib/components/Anime/carousel.dart
@@ -5,14 +5,14 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:text_scroll/text_scroll.dart';
 
 class Carousel extends StatelessWidget {
-   dynamic animeData;
+  dynamic animeData;
   final List<String> generes = ["Action", "Adventure", "Fantasy"];
 
   Carousel({super.key, this.animeData});
 
   @override
   Widget build(BuildContext context) {
-     if (animeData == null) {
+    if (animeData == null) {
       return const Center(child: CircularProgressIndicator());
     }
 
@@ -38,12 +38,15 @@ class Carousel extends StatelessWidget {
             builder: (BuildContext context) {
               return GestureDetector(
                 onTap: () {
-                  Navigator.pushNamed(context, '/detailspage',
-                      arguments: {"id": anime['id'], "image": anime['poster'], "tagg": anime['id'] + "MainCarousale"});
+                  Navigator.pushNamed(context, '/detailspage', arguments: {
+                    "id": anime['id'],
+                    "image": anime['poster'],
+                    "tagg": anime['id'] + "MainCarousale"
+                  });
                 },
                 child: Container(
                   width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                  margin: const EdgeInsets.symmetric(horizontal: 13),
                   decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.surfaceContainer,
                     borderRadius: BorderRadius.circular(20),
@@ -53,27 +56,40 @@ class Carousel extends StatelessWidget {
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        SizedBox(
-                          height: 280,
-                          width: 230,
-                          child: Hero(
-                            tag: anime['id'] + "MainCarousale",
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(20),
-                              child: CachedNetworkImage(
-                                imageUrl: anime['poster'],
-                                fit: BoxFit.cover,
-                                progressIndicatorBuilder:
-                                    (context, url, downloadProgress) =>
-                                        Center(
-                                          child: CircularProgressIndicator(
-                                              value: downloadProgress.progress),
-                                        ),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
+                        Stack(
+                          children: [
+                            SizedBox(
+                              height: 280,
+                              width: 230,
+                              child: Hero(
+                                tag: anime['id'] + "MainCarousale",
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10),
+                                  child: CachedNetworkImage(
+                                    imageUrl: anime['poster'],
+                                    fit: BoxFit.cover,
+                                    progressIndicatorBuilder:
+                                        (context, url, downloadProgress) =>
+                                            Center(
+                                      child: CircularProgressIndicator(
+                                          value: downloadProgress.progress),
+                                    ),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(Icons.error),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+                            Positioned(
+                                top: 0,
+                                left: 0,
+                                child: Container(
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(color: Theme.of(context).colorScheme.onPrimaryFixedVariant.withOpacity(0.8), borderRadius: BorderRadius.circular(10)),
+                                  child: Center(child: Text('#' + anime['rank'].toString(), style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),)),
+                                )),
+                          ],
                         ),
                         const SizedBox(height: 15),
                         TextScroll(
@@ -85,19 +101,20 @@ class Carousel extends StatelessWidget {
                           pauseBetween: const Duration(milliseconds: 1000),
                           textAlign: TextAlign.center,
                           selectable: true,
-                          style: const TextStyle(fontSize: 18),
+                          style: const TextStyle(fontSize: 16, fontFamily: "Poppins-Bold"),
                         ),
                         const SizedBox(height: 15),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: generes.map((item) {
                             return Container(
                               height: 30,
-                              margin: const EdgeInsets.only(
-                                  right:
-                                      4.0),
+                              margin: const EdgeInsets.only(right: 3.0),
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(20),
-                                 color: Theme.of(context).colorScheme.surfaceContainerHighest,
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .surfaceContainerHighest,
                               ),
                               child: Padding(
                                 padding:
@@ -105,8 +122,7 @@ class Carousel extends StatelessWidget {
                                 child: Center(
                                   child: Text(
                                     item,
-                                    style: const TextStyle(
-                                        fontSize: 12),
+                                    style: const TextStyle(fontSize: 12,  fontFamily: "Poppins-Bold"),
                                   ),
                                 ),
                               ),
@@ -118,7 +134,9 @@ class Carousel extends StatelessWidget {
                           width: 150,
                           height: 40,
                           decoration: BoxDecoration(
-                             color: Theme.of(context).colorScheme.surfaceContainerHighest,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .surfaceContainerHighest,
                             borderRadius: BorderRadius.circular(20),
                           ),
                           child: const Row(
@@ -126,8 +144,7 @@ class Carousel extends StatelessWidget {
                               children: [
                                 Text(
                                   "Watch now",
-                                  style: TextStyle(
-                                      fontSize: 16),
+                                  style: TextStyle(fontSize: 16, fontFamily: "Poppins-Bold"),
                                 ),
                                 SizedBox(
                                   width: 5,
