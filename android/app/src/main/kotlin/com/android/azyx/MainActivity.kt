f1d45c5acedde3083d73eb2d235541851ae48f60
Rey Yuuki <irfan31331@gmail.com>
2024-10-04 21:12:32 +0530

    - Changing App icon and removing unnecessary images and fonts

---
+++ android/app/src/main/kotlin/com/android/azyx/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.android.azyx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/daizy_tv/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.daizy_tv
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
