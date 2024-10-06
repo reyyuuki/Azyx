224676c68db0c0b0e6c136cc2c9094257701b8ef
Rey Yuuki <irfan31331@gmail.com>
2024-10-06 14:51:58 +0530

    - Fixing little bit styles

--- lib/settings/_theme_changer.dart
+++ lib/settings/_theme_changer.dart
@@ -39,7 +39,7 @@ class __ThemeChangeState extends State<ThemeChange> {
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
         title: const Text("App Themes",
-            style: TextStyle(fontWeight: FontWeight.bold)),
+            style: TextStyle(fontFamily: "Poppins-Bold")),
         leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
@@ -58,7 +58,7 @@ class __ThemeChangeState extends State<ThemeChange> {
             Container(
               height: 400,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(10),
                 color: Theme.of(context).colorScheme.surfaceContainerHighest,
               ),
               child: Column(
@@ -69,7 +69,7 @@ class __ThemeChangeState extends State<ThemeChange> {
                     child: Text(
                       "Mode",
                       style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          TextStyle(fontSize: 20, fontFamily: "Poppins-Bold"),
                     ),
                   ),
                   Mode(themeProvider: themeProvider),
@@ -77,98 +77,7 @@ class __ThemeChangeState extends State<ThemeChange> {
                     height: 140,
                     child: Padding(
                       padding: const EdgeInsets.all(10),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Text(
-                            "Highlight-Color",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Column(
-                                  children: [
-                                    GestureDetector(
-                                      onTap: () {
-                                        themeProvider.loadDynamicColors();
-                                      },
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                            border: Border.all(
-                                                width: 2,
-                                                color: isMaterial!
-                                                    ? Theme.of(context)
-                                                        .colorScheme
-                                                        .inverseSurface
-                                                    : Colors.transparent),
-                                            borderRadius:
-                                                BorderRadius.circular(20)),
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(3),
-                                          child: Container(
-                                            width: 25,
-                                            height: 25,
-                                            decoration: BoxDecoration(
-                                              gradient: LinearGradient(
-                                                  colors: [
-                                                    Theme.of(context)
-                                                        .colorScheme
-                                                        .primary,
-                                                          Theme.of(context)
-                                                        .colorScheme
-                                                        .secondary,
-                                                  ],
-                                                  begin: Alignment.topLeft,
-                                                  end: Alignment.bottomRight),
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 5,
-                                    ),
-                                    Text(
-                                      themeProvider.isMaterial! ? "Custom" : "",
-                                      style: const TextStyle(fontSize: 12),
-                                    )
-                                  ],
-                                ),
-                                ColorBall(
-                                  color: Colors.red,
-                                  name: "Red",
-                                ),
-                                ColorBall(
-                                  color: Colors.green,
-                                  name: "Green",
-                                ),
-                                ColorBall(color: Colors.yellow, name: "Yellow"),
-                                ColorBall(
-                                  color: Colors.indigo,
-                                  name: "Indigo",
-                                ),
-                                ColorBall(
-                                  color: Colors.purple,
-                                  name: "Purple",
-                                ),
-                                ColorBall(color: Colors.pink, name: "pink"),
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
+                      child: HighLight_Color(themeProvider: themeProvider, isMaterial: isMaterial),
                     ),
                   ),
                 ],
@@ -177,71 +86,170 @@ class __ThemeChangeState extends State<ThemeChange> {
             const SizedBox(
               height: 30,
             ),
-            Container(
-              height: 65,
-              decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.surfaceContainerHighest,
-                  borderRadius: BorderRadius.circular(20)),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Palette",
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Container(
-                        height: 50,
-                        width: 135,
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.inverseSurface,
-                            borderRadius: BorderRadius.circular(15)),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 6),
-                          child: DropdownButton(
-                            isExpanded: true,
-                            items: _items.map((String item) {
-                              return DropdownMenuItem<String>(
-                                value: item,
-                                child: Text(
-                                  item,
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .surface),
-                                ),
-                              );
-                            }).toList(),
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                _paletteValue = newValue;
-                              });
-                              if (newValue != null) {
-                                themeProvider.setPaletteColor(
-                                    _paletteValue!); 
-                              }
-                            },
-                            value: _paletteValue,
-                            underline: Container(),
-                            dropdownColor:
-                                Theme.of(context).colorScheme.inverseSurface,
-                            borderRadius: BorderRadius.circular(20),
-                            icon: Icon(
-                              Iconsax.arrow_down_1,
-                              color: Theme.of(context).colorScheme.surface,
-                            ),
-                          ),
-                        ))
-                  ],
-                ),
-              ),
-            )
+            palette(context, themeProvider)
           ],
         ),
       ),
+    );
+  }
+
+  Container palette(BuildContext context, ThemeProvider themeProvider) {
+    return Container(
+            height: 65,
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surfaceContainerHighest,
+                borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Palette",
+                    style:
+                        TextStyle(fontSize: 18, fontFamily: "Poppins-Bold"),
+                  ),
+                  Container(
+                      height: 50,
+                      width: 145,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.inverseSurface,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: DropdownButton(
+                          isExpanded: true,
+                          items: _items.map((String item) {
+                            return DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .surface),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              _paletteValue = newValue;
+                            });
+                            if (newValue != null) {
+                              themeProvider.setPaletteColor(
+                                  _paletteValue!); 
+                            }
+                          },
+                          value: _paletteValue,
+                          underline: Container(),
+                          dropdownColor:
+                              Theme.of(context).colorScheme.inverseSurface,
+                          borderRadius: BorderRadius.circular(10),
+                          icon: Icon(
+                            Iconsax.arrow_down_1,
+                            color: Theme.of(context).colorScheme.surface,
+                          ),
+                        ),
+                      ))
+                ],
+              ),
+            ),
+          );
+  }
+}
+
+class HighLight_Color extends StatelessWidget {
+  const HighLight_Color({
+    super.key,
+    required this.themeProvider,
+    required this.isMaterial,
+  });
+
+  final ThemeProvider themeProvider;
+  final bool? isMaterial;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          "Highlight-Color",
+          style: TextStyle(
+              fontSize: 20, fontFamily: "Poppins-Bold"),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      themeProvider.loadDynamicColors();
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 2,
+                              color: isMaterial!
+                                  ? Theme.of(context)
+                                      .colorScheme
+                                      .inverseSurface
+                                  : Colors.transparent),
+                          borderRadius:
+                              BorderRadius.circular(20)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(3),
+                        child: Container(
+                          width: 25,
+                          height: 25,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                                colors: [
+                                  Theme.of(context)
+                                      .colorScheme
+                                      .primary,
+                                        Theme.of(context)
+                                      .colorScheme
+                                      .secondary,
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight),
+                            borderRadius:
+                                BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    themeProvider.isMaterial! ? "Custom" : "",
+                    style: const TextStyle(fontSize: 12),
+                  )
+                ],
+              ),
+              ColorBall(color: Colors.red, name: "Red",),
+              ColorBall(color: Colors.green, name: "Green",),
+              ColorBall(color: Colors.yellow, name: "Yellow"),
+              ColorBall(color: Colors.indigo, name: "Indigo",),
+              ColorBall(color: Colors.purple, name: "Purple",),
+              ColorBall(color: Colors.pink, name: "pink"),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
