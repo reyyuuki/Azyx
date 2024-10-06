0504493caf98f74257fcdad5521b9aeb8c97397e
Rey Yuuki <irfan31331@gmail.com>
2024-10-06 14:49:40 +0530

    - Reducing code by using list tile

--- lib/components/setting_tile.dart
+++ lib/components/setting_tile.dart
@@ -5,40 +5,29 @@ class SettingTile extends StatelessWidget {
   final String name;
   final String routeName;
 
-  const SettingTile({super.key, required this.icon, required this.name, required this.routeName});
+  const SettingTile(
+      {super.key,
+      required this.icon,
+      required this.name,
+      required this.routeName});
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, routeName);
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Theme.of(context).colorScheme.surfaceContainerHighest,
+    return Container(
+      decoration: BoxDecoration(color: Theme.of(context).colorScheme.surfaceContainerHigh, borderRadius: BorderRadius.circular(10)),
+      child: ListTile(
+        title: Text(
+          name,
+          style: const TextStyle(fontFamily: "Poppins-Bold", ),
         ),
-        height: 50,
-        width: MediaQuery.of(context).size.width,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  icon, // Use the provided icon
-                  const SizedBox(width: 10),
-                  Text(name, style: const TextStyle(fontWeight: FontWeight.bold)), // Use the provided name
-                ],
-              ),
-              Transform.rotate(
-                angle: 3.14,
-                child: const Icon(Icons.arrow_back_ios),
-              ),
-            ],
-          ),
+        leading: icon,
+        trailing: Transform.rotate(
+          angle: 3.14,
+          child: const Icon(Icons.arrow_back_ios),
         ),
+        onTap: () {
+          Navigator.pushNamed(context, routeName);
+        },
       ),
     );
   }
