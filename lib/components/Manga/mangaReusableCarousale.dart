a8f2a66b23bd9f0f7d10bd7839ec6d0b841ef186
Rey Yuuki <irfan31331@gmail.com>
2024-10-12 11:20:04 +0530

    - Changing little bit styles and layout

--- lib/components/Manga/mangaReusableCarousale.dart
+++ lib/components/Manga/mangaReusableCarousale.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:ionicons/ionicons.dart';
+import 'package:iconsax/iconsax.dart';
 
 // ignore: must_be_immutable
 class Mangareusablecarousale extends StatelessWidget {
@@ -8,7 +8,8 @@ class Mangareusablecarousale extends StatelessWidget {
   final String? name;
   final String? taggName;
 
-  const Mangareusablecarousale({super.key, this.data, required this.name, required this.taggName});
+  const Mangareusablecarousale(
+      {super.key, this.data, required this.name, required this.taggName});
 
   @override
   Widget build(BuildContext context) {
@@ -16,98 +17,134 @@ class Mangareusablecarousale extends StatelessWidget {
       return const SizedBox();
     }
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
+    return Padding(
+      padding: const EdgeInsets.only(left: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(
-                Ionicons.grid,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
               Text(
                 name!,
-                style: const TextStyle(fontSize: 25,fontWeight: FontWeight.w600),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: "Poppins-Bold",
+                  foreground: Paint()
+                    ..shader = LinearGradient(
+                      colors: [
+                        Theme.of(context).colorScheme.inverseSurface,
+                        Theme.of(context).colorScheme.primary,
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ).createShader(const Rect.fromLTWH(0, 0, 200, 70)),
+                ),
+              ),
+              Icon(
+                Iconsax.arrow_right_4,
+                color: Theme.of(context).colorScheme.primary,
               ),
             ],
           ),
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        SizedBox(
-          height: 300,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: data!.length,
-            itemBuilder: (context, index) {
-              // Ensure data[index] has required fields
-              final item = data![index];
-              final imageUrl = item['image'] ?? '';
-              final title = item['title'] ?? '';
-              final id = item['id'] ?? '';
-              final tagg = id + taggName;
-
-              return GestureDetector(
-                onTap: () {
-                  if (id.isNotEmpty && imageUrl.isNotEmpty) {
-                    Navigator.pushNamed(
-                      context,
-                      '/mangaDetail',
-                      arguments: {"id": id, "image": imageUrl, "tagg": tagg},
-                    );
-                  } else {
-                    // Optionally show an error or perform an alternative action
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Error: Missing data')),
-                    );
-                  }
-                },
-                child: Column(
-                  children: [
-                    Container(
-                      height: 250,
-                      width: 150,
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Hero(
-                        tag: tagg,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: CachedNetworkImage(
-                            imageUrl: imageUrl,
-                            fit: BoxFit.cover,
-                            progressIndicatorBuilder: (context, url, downloadProgress) =>
-                                Center(
-                                  child: CircularProgressIndicator(
-                                    value: downloadProgress.progress,
-                                  ),
-                                ),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      title.length > 20
-                          ? '${title.substring(0, 17)}...'
-                          : title, style: const TextStyle(fontFamily: "Poppins-Bold"),
-                    ),
-                  ],
-                ),
-              );
-            },
+          const SizedBox(
+            height: 15,
           ),
-        ),
-      ],
+          SizedBox(
+            height: 190,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: data!.length,
+              itemBuilder: (context, index) {
+                final item = data![index];
+                final imageUrl = item['image'] ?? '';
+                final title = item['title'] ?? '';
+                final id = item['id'] ?? '';
+                final tagg = id + taggName;
+
+                return GestureDetector(
+                  onTap: () {
+                    if (id.isNotEmpty && imageUrl.isNotEmpty) {
+                      Navigator.pushNamed(
+                        context,
+                        '/mangaDetail',
+                        arguments: {"id": id, "image": imageUrl, "tagg": tagg},
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Error: Missing data')),
+                      );
+                    }
+                  },
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            height: 150,
+                            width: 103,
+                            margin: const EdgeInsets.only(right: 10),
+                            child: Hero(
+                              tag: tagg,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: CachedNetworkImage(
+                                  imageUrl: imageUrl,
+                                  fit: BoxFit.cover,
+                                  progressIndicatorBuilder:
+                                      (context, url, downloadProgress) =>
+                                          Center(
+                                    child: CircularProgressIndicator(
+                                      value: downloadProgress.progress,
+                                    ),
+                                  ),
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(Icons.error),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            child: Container(
+                              height: 28,
+                              width: 33,
+                              decoration: BoxDecoration(
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .surfaceContainerHighest
+                                    .withOpacity(0.8),
+                                borderRadius: const BorderRadius.only(
+                                    bottomRight: Radius.circular(10),
+                                    topLeft: Radius.circular(10)),
+                              ),
+                              child: Center(
+                                  child: Text(
+                                '# ${1 + index}',
+                                style:
+                                    const TextStyle(fontFamily: "Poppins-Bold"),
+                              )),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        title.length > 12
+                            ? '${title.substring(0, 10)}...'
+                            : title,
+                        style: const TextStyle(fontFamily: "Poppins-Bold"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
