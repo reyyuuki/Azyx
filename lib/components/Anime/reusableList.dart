775b89d33816299e15985779f33025668917d5b2
Rey Yuuki <irfan31331@gmail.com>
2024-10-12 11:18:33 +0530

    - Changing styles and designs

--- lib/components/Anime/reusableList.dart
+++ lib/components/Anime/reusableList.dart
@@ -1,95 +1,134 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:ionicons/ionicons.dart';
 
 // ignore: must_be_immutable
 class ReusableList extends StatelessWidget {
   dynamic data;
   final String? name;
-  final String? taggName; 
+  final String? taggName;
 
-  ReusableList({super.key, this.data, required this.name,required this.taggName});
+  ReusableList(
+      {super.key, this.data, required this.name, required this.taggName});
 
   @override
   Widget build(BuildContext context) {
     if (data == null) {
-      return const Center(child:CircularProgressIndicator());
+      return const Center(child: CircularProgressIndicator());
     }
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Row(
+    return Padding(
+      padding: const EdgeInsets.only(left: 15),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-               Icon(
-                Ionicons.grid,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
               Text(
                 name!,
-                style:  const TextStyle(fontSize: 25, fontFamily: "Poppins-Bold"),
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: "Poppins-Bold",
+                  foreground: Paint()
+                    ..shader = LinearGradient(
+                      colors: [
+                        Theme.of(context).colorScheme.inverseSurface,
+                        Theme.of(context).colorScheme.primary,
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ).createShader(const Rect.fromLTWH(0, 0, 200, 70)),
+                ),
+              ),
+              Icon(
+                Iconsax.arrow_right_4,
+                color: Theme.of(context).colorScheme.primary,
               ),
             ],
           ),
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-       SizedBox(
-          height: 300,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: data!.length,
-            itemBuilder: (context, index) {
-              final tagg = data[index]['id'] + taggName;
-              return GestureDetector(
-                 onTap: () {
-                  Navigator.pushNamed(context, '/detailspage',
-                      arguments:{"id": data[index]['id'], "image": data[index]['poster'], "tagg": tagg});
-                },
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 250,
-                        width: 150,
-                        margin: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Hero(
-                          tag: tagg,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: CachedNetworkImage(
-                                imageUrl: data[index]['poster'],
-                                fit: BoxFit.cover,
-                                progressIndicatorBuilder:
-                                    (context, url, downloadProgress) =>
-                                        Center(
-                                          child: CircularProgressIndicator(
-                                              value: downloadProgress.progress),
-                                        ),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(Icons.error),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: 190,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: data!.length,
+              itemBuilder: (context, index) {
+                final tagg = data[index]['id'] + taggName;
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/detailspage', arguments: {
+                        "id": data[index]['id'],
+                        "image": data[index]['poster'],
+                        "tagg": tagg
+                      });
+                    },
+                    child: Column(
+                      children: [
+                        Stack(
+                          children: [
+                            Container(
+                              height: 150,
+                              width: 103,
+                              margin: const EdgeInsets.only(right: 10),
+                              child: Hero(
+                                tag: tagg,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child: CachedNetworkImage(
+                                    imageUrl: data[index]['poster'],
+                                    fit: BoxFit.cover,
+                                    progressIndicatorBuilder:
+                                        (context, url, downloadProgress) =>
+                                            Center(
+                                      child: CircularProgressIndicator(
+                                          value: downloadProgress.progress),
+                                    ),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(Icons.error),
+                                  ),
+                                ),
                               ),
-                          ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              child: Container(
+                                height: 28,
+                                width: 33,
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .surfaceContainerHighest
+                                      .withOpacity(0.8),
+                                  borderRadius: const BorderRadius.only(
+                                      bottomRight: Radius.circular(10),
+                                      topLeft: Radius.circular(10)),
+                                ),
+                                child: Center(
+                                    child: Text(
+                                  '# ${1 +index}',
+                                  style: const TextStyle(
+                                      fontFamily: "Poppins-Bold"),
+                                )),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        data?[index]['name'].length > 20
-                            ? '${data![index]['name'].toString().substring(0, 17)}...'
-                            : data![index]['name'], style: const TextStyle(fontFamily: "Poppins-Bold"),
-                      )
-                    ],
-                  ));
-            },
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          data?[index]['name'].length > 12
+                              ? '${data![index]['name'].toString().substring(0, 10)}...'
+                              : data![index]['name'],
+                          style: const TextStyle(fontFamily: "Poppins-Bold"),
+                        )
+                      ],
+                    ));
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
