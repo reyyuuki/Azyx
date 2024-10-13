6c2588760e912e53925376f1f954a991e1f4d213
Rey Yuuki <irfan31331@gmail.com>
2024-10-13 18:17:46 +0530

    - Removing tab bar and added new drawer , some styling and layout changes

--- lib/pages/Lists/animelist.dart
+++ lib/pages/Lists/animelist.dart
@@ -1,5 +1,8 @@
-import 'package:animated_segmented_tab_control/animated_segmented_tab_control.dart';
+import 'package:daizy_tv/auth/auth_provider.dart';
+import 'package:daizy_tv/components/Anime/anime_lists.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:provider/provider.dart';
 
 class Animelist extends StatefulWidget {
   const Animelist({super.key});
@@ -9,58 +12,102 @@ class Animelist extends StatefulWidget {
 }
 
 class _AnimelistState extends State<Animelist> {
+  int _selectedIndex = 0; 
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
-    final textStyle = Theme.of(context).textTheme.bodyLarge;
-    final selectedTextStyle = textStyle?.copyWith(fontWeight: FontWeight.bold);
 
-    return DefaultTabController(
-      length: 5, // Number of tabs
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Anime List"),
+    return Consumer<AniListProvider>(
+    builder: (context, provide, child) {
+      final provider = provide.userData;
+      
+      if (provider['animeList'] == null) {
+        return const Center(child: CircularProgressIndicator());
+      }
+    final List<Map<String, dynamic>> categories = [
+      {"name": "Completed", "view": AnimeLists(data: provider['animeList'], status: "COMPLETED")},
+      {"name": "Planning", "view": AnimeLists(data: provider['animeList'], status: "PLANNING")},
+      {"name": "Continue", "view": AnimeLists(data: provider['animeList'], status: "CONTINUE")},
+      {"name": "Watched", "view": AnimeLists(data: provider['animeList'], status: "WATCHED")},
+      {"name": "Scheduled", "view": AnimeLists(data: provider['animeList'], status: "SCHEDULED")},
+      {"name": "Paused", "view": AnimeLists(data: provider['animeList'], status: "PAUSED")},
+    ];
+
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text(categories[_selectedIndex]['name'], style: const TextStyle(fontFamily: "Poppins-Bold")),
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Iconsax.menu_board5), 
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
         ),
-        body: SafeArea(
-          child: Column(
+      ),
+      drawer: Drawer(
+        width: 200,
+        child: Container(
+          decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface),
+          child: ListView(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SegmentedTabControl(
-                  // Customization of widget
-                  tabTextColor: Colors.black,
-                  selectedTabTextColor: Colors.white,  // Color of the selected indicator
-                  indicatorPadding: const EdgeInsets.all(0),
-                  squeezeIntensity: 1.5,
-                  height: 45,  
-                  tabPadding: const EdgeInsets.symmetric(horizontal: 12),
-                  textStyle: textStyle,
-                  selectedTextStyle: selectedTextStyle,
-                  // Tabs for anime categories
-                  tabs: const [
-                    SegmentTab(label: ""),
-                    SegmentTab(label: 'Planning'),
-                    SegmentTab(label: 'Continue'),
-                    SegmentTab(label: 'Watched'),
-                    SegmentTab(label: 'Scheduled'),
-                  ],
+              SizedBox(
+                height: 89,
+                child: DrawerHeader(
+                  child: Container(
+                    decoration: BoxDecoration(color: Theme.of(context).colorScheme.primaryFixedDim, borderRadius: BorderRadius.circular(10)),
+                    child: ListTile(
+                      leading: SizedBox(
+                        height: 35,
+                        width: 35,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.network(
+                            provider['avatar']['large'],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        provider['name'].length > 12 ? "${provider['name'].substring(0, 10)}..." : provider['name'],
+                        style: const TextStyle(fontFamily: "Poppins-Bold", color: Colors.black),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              const Expanded(
-                child: TabBarView(
-                  physics: BouncingScrollPhysics(),
-                  children: [
-                    Center(child: Text('Completed Anime List')),
-                    Center(child: Text('Planning to Watch List')),
-                    Center(child: Text('Continue Watching List')),
-                    Center(child: Text('Watched Anime List')),
-                    Center(child: Text('Scheduled Anime List')),
-                  ],
+              for (int i = 0; i < categories.length; i++)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: _selectedIndex == i ? Theme.of(context).colorScheme.inverseSurface : Theme.of(context).colorScheme.surfaceContainerHighest,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      leading: Icon(_selectedIndex == i ? Iconsax.main_component5 : Iconsax.main_component),
+                      title: Text(categories[i]['name'], style: const TextStyle(fontSize: 14, fontFamily: "Poppins-Bold")),
+                      selectedColor: Theme.of(context).colorScheme.surface,
+                      selected: _selectedIndex == i,
+                      onTap: () {
+                        setState(() {
+                          _selectedIndex = i;
+                        });
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ),
                 ),
-              ),
             ],
           ),
         ),
       ),
+      body: categories[_selectedIndex]['view'],
     );
   }
+    );
+}
 }
