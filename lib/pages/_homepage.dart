2249a9e160d90b10651c9e137a58958d658c9157
Rey Yuuki <irfan31331@gmail.com>
2024-10-15 13:24:37 +0530

    - Fixing not reloading  when login and logout problem

--- lib/pages/_homepage.dart
+++ lib/pages/_homepage.dart
@@ -1,9 +1,9 @@
-import 'dart:developer';
 
 import 'package:daizy_tv/auth/auth_provider.dart';
 import 'package:daizy_tv/backupData/anime.dart';
 import 'package:daizy_tv/backupData/manga.dart';
 import 'package:daizy_tv/components/Anime/reusableList.dart';
+import 'package:daizy_tv/components/Common/user_lists.dart';
 import 'package:daizy_tv/components/Header.dart';
 import 'package:daizy_tv/components/Manga/mangaReusableCarousale.dart';
 import 'package:daizy_tv/components/Recently-added/animeCarousale.dart';
@@ -27,11 +27,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     data();
-    
   }
-
-
-
 
   void data() {
     setState(() {
@@ -40,11 +36,11 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     var box = Hive.box("app-data");
+    final provider = Provider.of<AniListProvider>(context, listen: false);
+    
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Padding(
@@ -57,19 +53,14 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 20,
             ),
-      Row(
-        children: [
-          ElevatedButton(onPressed: () {
-            Navigator.pushNamed(context, './animelist');
-            Provider.of<AniListProvider>(context,listen: false).fetchUserAnimeList();
-          }, child: const Text("AnimeList")),
-          const SizedBox(width: 20,),
-          ElevatedButton(onPressed: () {
-            Navigator.pushNamed(context, './mangalist');
-            Provider.of<AniListProvider>(context,listen: false).fetchUserMangaList();
-          }, child: const Text("MangaList"))
-        ],
-      ),
+           Consumer<AniListProvider>(
+              builder: (context, provider, child) {
+                return provider.userData['name'] != null
+                    ? const UserLists()
+                    : const SizedBox.shrink();
+              },
+            ),
+            const SizedBox(height: 20,),
             ValueListenableBuilder(
               valueListenable: box.listenable(),
               builder: (context, Box<dynamic> box, _) {
@@ -78,11 +69,9 @@ class _HomePageState extends State<HomePage> {
                 return Animecarousale(carosaleData: animeWatches);
               },
             ),
-
             const SizedBox(
               height: 5,
             ),
-
             ValueListenableBuilder(
               valueListenable: box.listenable(),
               builder: (context, Box<dynamic> box, _) {
@@ -90,7 +79,6 @@ class _HomePageState extends State<HomePage> {
                 return Mangacarousale(carosaleData: readsmanga);
               },
             ),
-
             ReusableList(
                 name: "Recommend Animes",
                 taggName: "recommended",
