3e3099bfd0867db6446fecff483e0ddad859f112
Rey Yuuki <irfan31331@gmail.com>
2024-10-18 11:07:33 +0530

    - Adjusting rating errors and j name

--- lib/components/Anime/animeInfo.dart
+++ lib/components/Anime/animeInfo.dart
@@ -1,9 +1,9 @@
+import 'package:daizy_tv/components/Anime/genres.dart';
 import 'package:flutter/material.dart';
 
 class AnimeInfo extends StatelessWidget {
 final dynamic animeData;
-
-   const AnimeInfo({super.key,this.animeData});
+const AnimeInfo({super.key,this.animeData});
 
   @override
   Widget build(BuildContext context) {
@@ -11,68 +11,96 @@ final dynamic animeData;
       return const Center(child: CircularProgressIndicator());
     }
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          const SizedBox(
-            width: 100,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Aired: ",style: TextStyle( fontFamily: "Poppins-Bold")),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("Premiered: ",style: TextStyle( fontFamily: "Poppins-Bold")),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("Duration: ",style: TextStyle( fontFamily: "Poppins-Bold")),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("Status: ",style: TextStyle( fontFamily: "Poppins-Bold")),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("Rating: ",style: TextStyle( fontFamily: "Poppins-Bold")),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("Quality: ",style: TextStyle( fontFamily: "Poppins-Bold")),
-              ],
+    return Column(
+      children: [
+        Genres(genres: animeData['genres']),
+            const SizedBox(
+              height: 30,
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text(animeData['aired'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
               const SizedBox(
-                height: 5,
+                width: 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Japanese: ",style: TextStyle( fontFamily: "Poppins-Bold")),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("Aired: ",style: TextStyle( fontFamily: "Poppins-Bold")),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("Premiered: ",style: TextStyle( fontFamily: "Poppins-Bold")),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("Duration: ",style: TextStyle( fontFamily: "Poppins-Bold")),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("Status: ",style: TextStyle( fontFamily: "Poppins-Bold")),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("Rating: ",style: TextStyle( fontFamily: "Poppins-Bold")),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text("Quality: ",style: TextStyle( fontFamily: "Poppins-Bold")),
+                  ],
+                ),
               ),
-              Text(animeData['premiered'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
-              const SizedBox(
-                height: 5,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(animeData['japanese'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(animeData['aired'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(animeData['premiered'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(animeData['duration'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(animeData['status'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(animeData['rating'].toString(),style: const TextStyle( fontFamily: "Poppins-Bold")),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(animeData['quality'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
+                ],
               ),
-              Text(animeData['duration'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(animeData['status'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(animeData['rating'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
-              const SizedBox(
-                height: 5,
-              ),
-              Text(animeData['quality'] ?? "??",style: const TextStyle( fontFamily: "Poppins-Bold")),
             ],
           ),
-        ],
-      ),
+        ),
+        const SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                animeData['description'].length > 400
+                    ? animeData['description'].substring(0, 400) + '...'
+                    : animeData['description'],
+                    style: const TextStyle( fontFamily: "Poppins-Bold")
+              ),
+            ),
+      ],
     );
   }
 }
