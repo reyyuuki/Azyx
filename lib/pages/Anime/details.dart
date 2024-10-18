4badee8dd147ead6063bc22292f01c2b0a64bd60
Rey Yuuki <irfan31331@gmail.com>
2024-10-18 11:54:40 +0530

    - Removed unused imports

--- lib/pages/Anime/details.dart
+++ lib/pages/Anime/details.dart
@@ -6,7 +6,6 @@ import 'package:daizy_tv/components/Anime/floater.dart';
 import 'package:daizy_tv/components/Anime/poster.dart';
 import 'package:daizy_tv/components/Anime/coverImage.dart';
 import 'package:daizy_tv/scraper/Anilist/anilist_anime_details.dart';
-import 'package:daizy_tv/scraper/anime_detail_scraper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
