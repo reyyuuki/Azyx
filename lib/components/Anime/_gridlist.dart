692e22f5e8741c7afefe9ddb945d21e395f4dbce
Rey Yuuki <irfan31331@gmail.com>
2024-10-26 15:41:31 +0530

    - Changing the format of anime list with three cards per row and showing types and rating with new styles

--- lib/components/Anime/_gridlist.dart
+++ lib/components/Anime/_gridlist.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:daizy_tv/components/Anime/anime_item.dart';
 import 'package:flutter/material.dart';
 import 'package:text_scroll/text_scroll.dart';
 
@@ -20,7 +21,7 @@ class GridList extends StatelessWidget {
       child: GridView.builder( 
         shrinkWrap: true, 
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, 
+          crossAxisCount: 3, 
           crossAxisSpacing: 20.0, 
           childAspectRatio:
               0.56, 
@@ -28,52 +29,8 @@ class GridList extends StatelessWidget {
         itemCount: data!.length,
         itemBuilder: (context, index) {
           final item = data![index];
-          final tagg = item['id'] + "List";
-          return GestureDetector(
-            onTap: () => Navigator.pushNamed(context, '/detailspage',
-            arguments: {"id": item['id'], "image": item['poster'], "tagg": tagg}
-            ),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 250,
-                  width: 150,
-                  child: Hero(
-                    tag: tagg,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: CachedNetworkImage(
-                        imageUrl: item['poster'],
-                        fit: BoxFit.cover,
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) =>
-                                Center(
-                                  child: CircularProgressIndicator(
-                                      value: downloadProgress.progress),
-                                ),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: 150,
-                  child: TextScroll(
-                    item['name'],
-                    mode: TextScrollMode.bouncing,
-                    velocity: const Velocity(pixelsPerSecond: Offset(40, 0)),
-                    delayBefore: const Duration(milliseconds: 500),
-                    pauseBetween: const Duration(milliseconds: 1000),
-                    textAlign: TextAlign.center,
-                    selectable: true,
-                    style: const TextStyle(fontSize: 16, fontFamily: "Poppins-Bold"),
-                  ),
-                ),
-              ],
-            ),
-          );
+          final tagg = "${item['id']}List";
+          return AnimeItem(id: item['id'].toString(), poster: item['poster'], type: item['type'], name: item['name'], rating: item['rating'], tagg: tagg);
         },
       ),
     );
