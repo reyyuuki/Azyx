9abf89f8e15dd08f9f2e65376f9479610b6ca2dd
Rey Yuuki <irfan31331@gmail.com>
2024-11-04 18:36:50 +0530

    - Improving grid design of manga

--- lib/components/Manga/_manga_grid.dart
+++ lib/components/Manga/_manga_grid.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:daizy_tv/components/Anime/anime_item.dart';
 import 'package:flutter/material.dart';
 import 'package:text_scroll/text_scroll.dart';
 
@@ -28,54 +29,10 @@ class MangaGrid extends StatelessWidget {
         itemCount: data!.length,
         itemBuilder: (context, index) {
           final item = data![index];
-          final tagg = item['id'] + "List";
-          return GestureDetector(
-            onTap: () => Navigator.pushNamed(context, '/mangaDetail',
-            arguments: {"id": item['id'], "image": item['image'], "tagg": tagg}
-            ),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 250,
-                  width: 150,
-                  child: Hero(
-                    tag: tagg,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10),
-                      child: CachedNetworkImage(
-                        imageUrl: item['image'],
-                        fit: BoxFit.cover,
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) =>
-                                Center(
-                                  child: CircularProgressIndicator(
-                                      value: downloadProgress.progress),
-                                ),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                SizedBox(
-                  width: 150,
-                  child: TextScroll(
-                    item['title'],
-                    mode: TextScrollMode.bouncing,
-                    velocity: const Velocity(pixelsPerSecond: Offset(40, 0)),
-                    delayBefore: const Duration(milliseconds: 500),
-                    pauseBetween: const Duration(milliseconds: 1000),
-                    textAlign: TextAlign.center,
-                    selectable: true,
-                    style: const TextStyle(fontSize: 16, fontFamily: "Poppins-Bold"),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+          final tagg = "${item['id']}List";
+          return ItemCard(id: item['id'].toString(), poster: item['poster'], type: item['type'], name: item['name'], rating: item['averageScore'], tagg: tagg, route: '/detailspage',);
+        }
+      )
     );
   }
 }
