1aa06f0215d6acd772f06e25d919e61966b1107d
Rey Yuuki <irfan31331@gmail.com>
2024-11-04 18:41:25 +0530

    - Added fallback data

--- lib/Screens/Anime/animePage.dart
+++ lib/Screens/Anime/animePage.dart
@@ -1,6 +1,6 @@
-
 import 'package:daizy_tv/auth/auth_provider.dart';
-import 'package:daizy_tv/backupData/anime.dart';
+import 'package:daizy_tv/backupData/anilist_anime.dart';
+import 'package:daizy_tv/components/Anime/anime_carousale.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:daizy_tv/components/Anime/carousel.dart';
@@ -23,8 +23,24 @@ class _HomepageState extends State<Animepage> {
   dynamic topUpComingAnime;
   dynamic baseData;
   var box = Hive.box("app-data");
- 
- @override
+
+  @override
+  void initState() {
+    super.initState();
+    backupData();
+  }
+
+  void backupData() {
+    setState(() {
+      mostPopularAnimes = fallbackAnilistData['data']['popularAnimes']['media'];
+      trendingAnime = fallbackAnilistData['data']['trendingAnimes']['media'];
+      latestEpisodesAnime =
+          fallbackAnilistData['data']['latestAnimes']['media'];
+      topUpComingAnime = fallbackAnilistData['data']['top10Today']['media'];
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
@@ -32,16 +48,13 @@ class _HomepageState extends State<Animepage> {
         padding: const EdgeInsets.all(8.0),
         child: Consumer<AniListProvider>(
           builder: (context, provider, child) {
-            if (provider.anilistData.isEmpty) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            
+           
             mostPopularAnimes = provider.anilistData['popular'];
             trendingAnime = provider.anilistData['trending'];
             latestEpisodesAnime = provider.anilistData['latest'];
             topUpComingAnime = provider.anilistData['completed'];
 
-            return ListView(
+          return ListView(
               children: [
                 const Header(
                   name: "Anime",
@@ -71,14 +84,15 @@ class _HomepageState extends State<Animepage> {
                           borderRadius: BorderRadius.circular(20),
                           borderSide: BorderSide.none,
                         ),
-                        fillColor: Theme.of(context).colorScheme.surfaceContainer,
+                        fillColor:
+                            Theme.of(context).colorScheme.surfaceContainer,
                         filled: true,
                       ),
                     ),
                   ),
                 ),
                 const SizedBox(height: 30.0),
-                Carousel(
+                AnimeCarousale(
                   animeData: trendingAnime,
                 ),
                 const SizedBox(height: 30.0),
@@ -86,25 +100,28 @@ class _HomepageState extends State<Animepage> {
                   name: 'Popular Animes',
                   data: mostPopularAnimes,
                   taggName: "Carousale1",
+                  route: '/detailspage',
                 ),
                 const SizedBox(height: 10),
                 ReusableList(
                   name: 'Latest Episodes',
                   data: latestEpisodesAnime,
                   taggName: "Carousale2",
+                  route: '/detailspage',
                 ),
                 const SizedBox(height: 10),
                 ReusableList(
                   name: 'Top Upcoming',
                   data: topUpComingAnime,
                   taggName: "Carousale3",
+                  route: '/detailspage',
                 ),
                 const SizedBox(height: 10),
               ],
-            );
-          },
-        ),
-      ),
+                              );
+          }
+      )
+      )
     );
   }
 }
