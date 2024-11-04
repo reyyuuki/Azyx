fd4212c5511bc85c401e06f8eb9a08866ead2674
Rey Yuuki <irfan31331@gmail.com>
2024-11-04 18:32:46 +0530

    - Some ui adjustment and routing changes to manga and anime

--- lib/components/Anime/_gridlist.dart
+++ lib/components/Anime/_gridlist.dart
@@ -1,14 +1,14 @@
-import 'package:cached_network_image/cached_network_image.dart';
+
 import 'package:daizy_tv/components/Anime/anime_item.dart';
 import 'package:flutter/material.dart';
-import 'package:text_scroll/text_scroll.dart';
 
 class GridList extends StatelessWidget {
   const GridList({
     super.key,
     required this.data,
+    required this.route
   });
-
+  final route;
   final dynamic data;
 
   @override
@@ -16,22 +16,24 @@ class GridList extends StatelessWidget {
     if(data == null){
       return const SizedBox.shrink();
     }
-    return Container(
-      color: Theme.of(context).colorScheme.surface,
-      child: GridView.builder( 
-        shrinkWrap: true, 
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, 
-          crossAxisSpacing: 20.0, 
-          childAspectRatio:
-              0.56, 
+    return Padding(
+      padding: const EdgeInsets.only(left: 12),
+      child: Container(
+        color: Theme.of(context).colorScheme.surface,
+        child: GridView.builder(  
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, 
+            crossAxisSpacing: 5.0, 
+            childAspectRatio:
+                0.56, 
+          ),
+          itemCount: data!.length,
+          itemBuilder: (context, index) {
+            final item = data![index];
+            final tagg = "${item['id']}List";
+            return ItemCard(id: item['id'].toString(), poster: item['poster'], type: item['type'], name: item['name'], rating: item['averageScore'], tagg: tagg, route: route,);
+          },
         ),
-        itemCount: data!.length,
-        itemBuilder: (context, index) {
-          final item = data![index];
-          final tagg = "${item['id']}List";
-          return AnimeItem(id: item['id'].toString(), poster: item['poster'], type: item['type'], name: item['name'], rating: item['rating'], tagg: tagg);
-        },
       ),
     );
   }
