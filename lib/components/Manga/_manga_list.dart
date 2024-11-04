9b90d3eb9c63bec7ba02e185b9d046a65cd010cb
Rey Yuuki <irfan31331@gmail.com>
2024-11-04 18:37:29 +0530

    - some ui changes and anilist data integrated

--- lib/components/Manga/_manga_list.dart
+++ lib/components/Manga/_manga_list.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class MangaSearchList extends StatelessWidget {
   dynamic data;
@@ -15,14 +16,14 @@ class MangaSearchList extends StatelessWidget {
         itemCount: data.length,
         itemBuilder: (context, index) {
           final item = data[index];
-          final title = item['title'];
-          final image = item['image'];
-          final id = item['id'];
-          final tagg = id + "List";
+          final title = item['name'];
+          final image = item['poster'];
+          final id = item['id'].toString();
+          final tagg = "${id}List";
           return GestureDetector(
             onTap: () {
               Navigator.pushNamed(context, "/mangaDetail",
-                  arguments: {"id": id, "image": image, "tagg":tagg});
+                  arguments: {"id": id, "image": image, "tagg": tagg});
             },
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -32,17 +33,20 @@ class MangaSearchList extends StatelessWidget {
                     height: 200,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        gradient: LinearGradient(colors: [
-                          Colors.transparent,
-                          Theme.of(context).colorScheme.primary
-                        ], begin: Alignment.center, end: Alignment.bottomCenter)),
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.transparent,
+                              Theme.of(context).colorScheme.primary
+                            ],
+                            begin: Alignment.center,
+                            end: Alignment.bottomCenter)),
                     width: MediaQuery.of(context).size.width,
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(20),
                       child: ImageFiltered(
                         imageFilter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
                         child: CachedNetworkImage(
-                          imageUrl: image,
+                          imageUrl: item['cover'] ?? image,
                           fit: BoxFit.cover,
                           progressIndicatorBuilder:
                               (context, url, downloadProgress) => Center(
@@ -65,7 +69,7 @@ class MangaSearchList extends StatelessWidget {
                           gradient: LinearGradient(
                             colors: [
                               Colors.transparent,
-                              Theme.of(context).colorScheme.primary,
+                              Theme.of(context).colorScheme.surface,
                             ],
                             begin: Alignment.topCenter,
                             end: Alignment.bottomCenter,
@@ -81,32 +85,155 @@ class MangaSearchList extends StatelessWidget {
                             horizontal: 10, vertical: 15),
                         child: Row(
                           children: [
-                            SizedBox(
-                              height: 170,
-                              width: 120,
-                              child: Hero(
-                                tag: tagg,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: CachedNetworkImage(
-                                    imageUrl: image,
-                                    fit: BoxFit.cover,
+                            Stack(
+                              children: [
+                                Container(
+                                  height: 150,
+                                  width: 103,
+                                  margin: const EdgeInsets.only(right: 10),
+                                  child: Hero(
+                                    tag: tagg,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: CachedNetworkImage(
+                                        imageUrl: image,
+                                        fit: BoxFit.cover,
+                                        progressIndicatorBuilder:
+                                            (context, url, downloadProgress) =>
+                                                Center(
+                                          child: CircularProgressIndicator(
+                                              value: downloadProgress.progress),
+                                        ),
+                                        errorWidget: (context, url, error) =>
+                                            const Icon(Icons.error),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
+                                item['type'].isNotEmpty
+                                    ? Positioned(
+                                        top: 0,
+                                        left: 0,
+                                        child: Container(
+                                          height: 22,
+                                          decoration: BoxDecoration(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primary,
+                                            borderRadius: const BorderRadius.only(
+                                                bottomRight:
+                                                    Radius.circular(15),
+                                                topLeft: Radius.circular(15)),
+                                          ),
+                                          child: Center(
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      horizontal: 5),
+                                              child: Text(
+                                                item['type'],
+                                                style: TextStyle(
+                                                    fontSize: 12,
+                                                    color: Theme.of(context)
+                                                        .colorScheme
+                                                        .onPrimaryFixedVariant,
+                                                    fontFamily: "Poppins-Bold"),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    : const SizedBox.shrink(),
+                                item['averageScore'] != null
+                                    ? Positioned(
+                                        bottom: 0,
+                                        right: 0,
+                                        child: Container(
+                                          height: 22,
+                                          margin:
+                                              const EdgeInsets.only(right: 10),
+                                          decoration: BoxDecoration(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .primary,
+                                            borderRadius: const BorderRadius.only(
+                                                bottomRight:
+                                                    Radius.circular(10),
+                                                topLeft: Radius.circular(25)),
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                horizontal: 8),
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.end,
+                                              children: [
+                                                Text(
+                                                  item['averageScore'] != null
+                                                      ? (item['averageScore']! /
+                                                              10)
+                                                          .toString()
+                                                      : "N/A",
+                                                  style: TextStyle(
+                                                      fontSize: 12,
+                                                      color: Theme.of(context)
+                                                          .colorScheme
+                                                          .onPrimaryFixedVariant,
+                                                      fontFamily:
+                                                          "Poppins-Bold"),
+                                                ),
+                                                Icon(
+                                                  Iconsax.star1,
+                                                  size: 16,
+                                                  color: Theme.of(context)
+                                                      .colorScheme
+                                                      .onPrimaryFixedVariant,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    : const SizedBox.shrink(),
+                                item['status'] != null &&
+                                        (item['status'] == "RELEASING" ||
+                                            item['status'] == "Ongoing")
+                                    ? Positioned(
+                                        bottom: 0,
+                                        left: 0,
+                                        child: Container(
+                                          height: 15,
+                                          width: 15,
+                                          decoration: BoxDecoration(
+                                              color: const Color.fromARGB(
+                                                  255, 95, 209, 99),
+                                              border: Border.all(
+                                                  width: 2,
+                                                  color: const Color.fromARGB(
+                                                      255, 8, 117, 11)),
+                                              borderRadius:
+                                                  BorderRadius.circular(20)),
+                                        ))
+                                    : const SizedBox.shrink()
+                              ],
                             ),
                             const SizedBox(
                               width: 20,
                             ),
                             SizedBox(
                                 width: 150,
-                                child: Text(
-                                  title.length > 50
-                                      ? title.substring(0, 47) + "..."
-                                      : title,
-                                  style: const TextStyle(
-                                      fontFamily: "Poppins-Bold",
-                                      fontSize: 16),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      title.length > 50
+                                          ? title.substring(0, 47) + "..."
+                                          : title,
+                                      style: const TextStyle(
+                                          fontFamily: "Poppins-Bold", fontSize: 16),
+                                    ),
+                                    Text('${item['chapters']} | ${item['volumes']}' )
+                                  ],
                                 )),
                           ],
                         ),
