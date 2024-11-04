7cb7291f64738aa6fe75ea433d2ff8b63351bbec
Rey Yuuki <irfan31331@gmail.com>
2024-11-04 18:45:40 +0530

    - Fixed new data integrated dynamically and next or prev problem

--- lib/Screens/Manga/read.dart
+++ lib/Screens/Manga/read.dart
@@ -1,77 +1,84 @@
-import 'dart:convert';
 import 'dart:developer';
-
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:daizy_tv/Provider/manga_sources.dart';
 import 'package:daizy_tv/components/Manga/sliderBar.dart';
 import 'package:daizy_tv/Hive_Data/appDatabase.dart';
-import 'package:daizy_tv/utils/scraper/mangakakalot/manga_scrapper.dart';
+import 'package:daizy_tv/utils/scraper/Manga/Manga_Sources/extract_class.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
-
-
 
 class Read extends StatefulWidget {
   final String mangaId;
-  String chapterId;
+  String chapterLink;
   String image;
 
-   Read({super.key,required this.mangaId,required this.chapterId,required this.image});
+  Read({
+    super.key,
+    required this.mangaId,
+    required this.chapterLink,
+    required this.image,
+  });
 
   @override
   State<Read> createState() => _ReadState();
 }
 
 class _ReadState extends State<Read> {
-  List<dynamic>? chapterList;
+  dynamic chapterData;
   List<dynamic>? chapterImages;
   String? currentChapter;
   String? mangaTitle;
-  int? index;
-  
-  bool show = true;
+  String? url;
+  ExtractClass? source;
+
   bool isLoading = true;
   bool hasError = false;
+  bool isNext = true;
+  bool isPrev = true;
 
   @override
   void initState() {
     super.initState();
-    log(widget.chapterId);
     fetchChapterData();
   }
 
   final ScrollController _scrollController = ScrollController();
 
   Future<void> fetchChapterData() async {
+    final provider = Provider.of<MangaSourcesProvider>(context, listen: false);
+    source = provider.instance;
     try {
-      final resp = await fetchChapterDetails(mangaId: widget.mangaId, chapterId: widget.chapterId);
-      final provider = Provider.of<Data>(context, listen: false);
-      if (resp.toString().isNotEmpty) {
-        setState(() {
-          chapterList = resp['chapterListIds'];
-          chapterImages = resp['images'];
-          currentChapter = resp['currentChapter'];
-          mangaTitle = resp['title'];
-          index = resp['chapterListIds']
-              ?.indexWhere((chapter) => chapter['name'] == currentChapter);
-          isLoading = false;
-        });
-        provider.addReadsManga(
+      if (source != null) {
+        final resp = await source!.fetchPages(widget.chapterLink);
+        final dataProvider = Provider.of<Data>(context, listen: false);
+        
+        if (resp != null && resp.toString().isNotEmpty) {
+          setState(() {
+            chapterImages = resp['images'];
+            currentChapter = resp['currentChapter'];
+            mangaTitle = resp['title'];
+            chapterData = resp;
+            isLoading = false;
+            isNext = resp['nextChapter'] != null;
+            isPrev = resp['previousChapter'] != null;
+          });
+
+          dataProvider.addReadsManga(
             mangaId: widget.mangaId,
             mangaTitle: resp['title'],
             currentChapter: currentChapter.toString(),
-            mangaImage: widget.image);
-            log(chapterImages.toString());
-      } else {
-        setState(() {
-          hasError = true;
-          isLoading = false;
-        });
-        log('error');
+            mangaImage: widget.image,
+          );
+        } else {
+          setState(() {
+            hasError = true;
+            isLoading = false;
+          });
+          log('Error: Chapter data is empty');
+        }
       }
     } catch (e) {
-      log(e.toString());
+      log('Error: $e');
       setState(() {
         hasError = true;
         isLoading = false;
@@ -79,25 +86,29 @@ class _ReadState extends State<Read> {
     }
   }
 
-    Future<void> fetchChapterImages() async {
+  Future<void> fetchChapterImages() async {
     setState(() {
       isLoading = true;
     });
     try {
-      final provider = Provider.of<Data>(context, listen: false);
-          final data = await fetchChapterDetails(mangaId: widget.mangaId, chapterId: chapterList?[index!]['id']);
-      if (data.toString().isNotEmpty) {
+      final dataProvider = Provider.of<Data>(context, listen: false);
+      final data = await source!.fetchPages(url!);
+      if (data != null && data.toString().isNotEmpty) {
         setState(() {
           chapterImages = data['images'];
           currentChapter = data['currentChapter'];
+          isNext = data['nextChapter'] != null;
+          isPrev = data['previousChapter'] != null;
+          chapterData = data;
           isLoading = false;
         });
-        provider.addReadsManga(
-            mangaId: widget.mangaId,
-            mangaTitle: mangaTitle!,
-            currentChapter: currentChapter.toString(),
-            mangaImage: widget.image)
-            ;
+
+        dataProvider.addReadsManga(
+          mangaId: widget.mangaId,
+          mangaTitle: mangaTitle!,
+          currentChapter: currentChapter.toString(),
+          mangaImage: widget.image,
+        );
       } else {
         setState(() {
           hasError = true;
@@ -105,7 +116,7 @@ class _ReadState extends State<Read> {
         });
       }
     } catch (e) {
-      log(e.toString());
+      log('Error: $e');
       setState(() {
         hasError = true;
         isLoading = false;
@@ -113,34 +124,41 @@ class _ReadState extends State<Read> {
     }
   }
 
-  void handleChapter(String ? direction) {
+ void handleChapter(String? direction) async {
+  if ((direction == 'right' && chapterData['nextChapter'] != null) ||
+      (direction == 'left' && chapterData['previousChapter'] != null)) {
+        log(direction == 'right '? 'isright' : 'isleft');
+        setState(() {
+          url = direction == 'right' ? chapterData['nextChapter'] : chapterData['previousChapter'];
+        });
+ 
+    setState(() {
+      isNext = false;
+      isPrev = false;
+      isLoading = true;
+    });
 
-     if (direction == 'right') {
-      index = ((chapterList?.indexWhere(
-                  (chapter) => chapter['name'] == currentChapter))! -
-              1)
-          .clamp(0, chapterList!.length - 1);
-          log(index.toString());
-    } else {
-      index = ((chapterList?.indexWhere(
-                  (chapter) => chapter['name'] == currentChapter))! +
-              1)
-          .clamp(0, chapterList!.length - 1);
-    }
-    fetchChapterImages();
+    await fetchChapterImages();
+
+    setState(() {
+      isNext = chapterData['nextChapter'] != null;
+      isPrev = chapterData['previousChapter'] != null;
+    });
+
   }
+}
 
 
   @override
   Widget build(BuildContext context) {
-    
-
     return Sliderbar(
       title: isLoading ? '??' : mangaTitle ?? "Unknown",
       chapter: isLoading ? 'Chapter ?' : currentChapter ?? "Unknown",
       totalImages: chapterImages?.length ?? 0,
       scrollController: _scrollController,
       handleChapter: handleChapter,
+      isNext: isNext,
+      isPrev: isPrev,
       child: Center(
         child: isLoading
             ? const CircularProgressIndicator()
@@ -150,25 +168,26 @@ class _ReadState extends State<Read> {
                     controller: _scrollController,
                     itemCount: chapterImages!.length,
                     itemBuilder: (context, index) {
-                       return CachedNetworkImage(
+                      return CachedNetworkImage(
+                        httpHeaders: {'Referer': widget.chapterLink},
                         imageUrl: chapterImages![index]['image'],
                         fit: BoxFit.cover,
                         placeholder: (context, progress) => SizedBox(
                           height: MediaQuery.of(context).size.height,
                           width: MediaQuery.of(context).size.width,
                           child: const Center(
-                              child: SizedBox(
-                                  height: 50,
-                                  width: 50,
-                                  child: CircularProgressIndicator())),
+                            child: SizedBox(
+                              height: 50,
+                              width: 50,
+                              child: CircularProgressIndicator(),
+                            ),
+                          ),
                         ),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
+                        errorWidget: (context, url, error) => const Icon(Icons.error),
                       );
                     },
                   ),
       ),
     );
   }
-
 }
