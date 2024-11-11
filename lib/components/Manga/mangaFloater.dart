9702834b458cb1575547d44a114b697c3ee44b4b
Rey Yuuki <irfan31331@gmail.com>
2024-11-12 00:05:25 +0530

    - Added favourites and continue features

--- lib/components/Manga/mangaFloater.dart
+++ lib/components/Manga/mangaFloater.dart
@@ -1,110 +1,133 @@
-import 'dart:developer';
-import 'dart:ui';
+// ignore_for_file: file_names
 
 import 'package:daizy_tv/Hive_Data/appDatabase.dart';
+import 'package:daizy_tv/utils/scraper/Manga/Manga_Extenstions/extract_class.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:provider/provider.dart';
-import 'package:text_scroll/text_scroll.dart';
 
-class Mangafloater extends StatelessWidget {
-final String? id;
-final String? image;
-final String? title;
-
-  const Mangafloater({super.key,required this.id, this.image, this.title});
+// ignore: must_be_immutable
+class Mangafloater extends StatefulWidget {
+  Map<String, dynamic> data;
+  String currentLink;
+  String currentChapter;
+  List<Map<String, dynamic>> chapterList;
+  ExtractClass source;
+  Mangafloater({super.key, required this.data, required this.currentLink, required this.chapterList,required this.source, required this.currentChapter});
 
   @override
-  Widget build(BuildContext context) {    
-     final provider = Provider.of<Data>(context);
-    final currentChapter =
-        provider.getCurrentChapterForManga(id!) ?? 'chapter-1';
+  State<Mangafloater> createState() => _MangafloaterState();
+}
 
+class _MangafloaterState extends State<Mangafloater> {
+  bool isFavrouite = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<Data>(context, listen: false);
+    isFavrouite = provider.getFavroite(widget.data['id']);
     return Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: Container(
-            height: 60,
-            margin: const EdgeInsets.all(20),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(
-                  sigmaX: 5,
-                  sigmaY: 5,
-                ),
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  decoration:
-                      BoxDecoration(color: Colors.white.withOpacity(0.1)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            constraints: const BoxConstraints(maxWidth: 130),
-                            child: TextScroll(
-                              title!,
-                              mode: TextScrollMode.bouncing,
-                              velocity: const Velocity(
-                                  pixelsPerSecond: Offset(20, 0)),
-                              delayBefore: const Duration(milliseconds: 500),
-                              pauseBetween:
-                                  const Duration(milliseconds: 1000),
-                              textAlign: TextAlign.center,
-                              selectable: true,
-                              style: const TextStyle( fontFamily: "Poppins-Bold")
-                            ),
-                          ),
-                          Text(
-                            currentChapter.isEmpty
-                                  ? 'Chapter 1'
-                                  : currentChapter,
-                            style: const TextStyle(
-                                color: Color.fromARGB(187, 141, 135, 135), fontFamily: "Poppins-Bold"),
-                          ),
-                        ],
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/read',
-                              arguments: {"mangaId": id, "chapterId":currentChapter, "image": image});
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor:  Theme.of(context).colorScheme.inverseSurface,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child:  Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Icon(
-                              Ionicons.book,
-                               color: Theme.of(context).colorScheme.inversePrimary, // Icon color
-                            ),
-                            const SizedBox(width: 8),
-                            Text(
-                              'Read',
-                              style: TextStyle(
-                                 color: Theme.of(context).colorScheme.surface,
-                                 fontFamily: "Poppins-Bold" // Text color
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AnimatedContainer(
+             duration: const Duration(milliseconds: 500),
+        width: !isFavrouite ? MediaQuery.of(context).size.width * 0.5 : 100,
+        curve: Curves.easeInOut,
+            child: GestureDetector(
+              onTap: () {
+                if (!isFavrouite) {
+                 provider.addFavrouiteManga(
+                          title: widget.data['name'],
+                          id: widget.data['id'],
+                          image: widget.data['poster'],
+                          chapterList: widget.chapterList,
+                          currentLink: widget.currentLink,
+                          currentChapter: widget.currentChapter,
+                          description: widget.data['description'],
+                          source: widget.source
+                        );
+                  setState(() {
+                    isFavrouite = true;
+                  });
+                } else {
+                   provider.removefavrouite(widget.data['id']);
+                  setState(() {
+                    isFavrouite = false;
+                  });
+                }
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surfaceContainer,
+                borderRadius: const BorderRadius.only(topLeft: Radius.circular(20))
+              ),
+                height: 60,
+                width:
+                    !isFavrouite ? MediaQuery.of(context).size.width * 0.5 : 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      isFavrouite ? Icons.favorite : Icons.favorite_border,
+                      color: Colors.red,
+                    ),
+                    const SizedBox(
+                      width: 5,
+                    ),
+                    !isFavrouite
+                        ? const Text(
+                            "Favrouite",
+                            style: TextStyle(fontFamily: "Poppins-Bold"),
+                          )
+                        : const SizedBox.shrink()
+                  ],
                 ),
               ),
             ),
           ),
-        );
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/read', arguments: {
+                  'mangaId': widget.data['id'],
+                  'image': widget.data['poster'],
+                  'chapterLink': widget.currentLink
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                borderRadius: const BorderRadius.only(topRight: Radius.circular(20))
+              ),
+                height: 60,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Ionicons.book_outline,
+                      color:
+                          Theme.of(context).colorScheme.primary, // Icon color
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      '${widget.currentChapter}',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontFamily: "Poppins-Bold",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
