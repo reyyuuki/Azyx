f782a7804c93628ec107a95d1533d1acfa915f7d
Rey Yuuki <irfan31331@gmail.com>
2024-11-26 08:25:39 +0530

    refactor: implementing the source handlers in provider

---
+++ lib/Provider/sources_provider.dart
@@ -0,0 +1,82 @@
+
+import 'package:daizy_tv/utils/sources/Anime/Base/base_class.dart';
+import 'package:daizy_tv/utils/sources/Anime/Extensions/hianime_api.dart';
+import 'package:daizy_tv/utils/sources/Anime/Extensions/hianime_scrapper.dart';
+import 'package:daizy_tv/utils/sources/Anime/SourceHandler/sourcehandler.dart';
+import 'package:daizy_tv/utils/sources/Manga/Base/extract_class.dart';
+import 'package:daizy_tv/utils/sources/Manga/Extenstions/mangaKakalot.dart';
+import 'package:daizy_tv/utils/sources/Manga/Extenstions/manga_bat.dart';
+import 'package:daizy_tv/utils/sources/Manga/Extenstions/manga_nato.dart';
+import 'package:daizy_tv/utils/sources/Manga/Extenstions/mangakakalot_unofficial.dart';
+import 'package:daizy_tv/utils/sources/Manga/SourceHandler/sourcehandler.dart';
+import 'package:daizy_tv/utils/sources/Novel/SourceHandler/novel_sourcehandler.dart';
+import 'package:daizy_tv/utils/sources/Novel/base/base_class.dart';
+import 'package:daizy_tv/utils/sources/Novel/Extensions/novel_buddy.dart';
+import 'package:daizy_tv/utils/sources/Novel/Extensions/wuxia_novel.dart';
+import 'package:flutter/material.dart';
+
+ class SourcesProvider with ChangeNotifier {
+
+  ExtractClass _instance = MangakakalotUnofficial();
+  EXtractAnimes _animeInstance = HianimeApi();
+  NovelSourceBase _novelInstance = NovelBuddy();
+  List<ExtractClass>? _sources;
+  List<EXtractAnimes>? _animeSources;
+  List<NovelSourceBase>? _novelSource;
+  AnimeSourcehandler animehandler = AnimeSourcehandler();
+  MangaSourcehandler mangaSourcehandler = MangaSourcehandler();
+  NovelSourcehandler novelSourcehandler = NovelSourcehandler();
+
+  ExtractClass get instance => _instance;
+  List<ExtractClass>? get sources => _sources;
+  EXtractAnimes get animeInstance => _animeInstance;
+  List<EXtractAnimes>? get animeSources => _animeSources;
+  NovelSourceBase get novelInstance => _novelInstance;
+  List<NovelSourceBase>? get novelSource => _novelSource;
+  
+
+  Future<void> allMangaSources() async {
+    _sources = [];
+   _sources?.add(MangakakalotUnofficial());
+   _sources?.add(MangaNato());
+   _sources?.add(MangaKakalot());
+   _sources?.add(MangaBat());
+
+   _animeSources = [];
+   _animeSources?.add(HianimeApi());
+   _animeSources?.add(HianimeScrapper());
+
+   _novelSource = [];
+  _novelSource?.add(NovelBuddy());
+  _novelSource?.add(WuxiaClick());
+   notifyListeners();
+  }
+
+
+  void setInstance(ExtractClass instance) {
+    _instance = instance;
+    notifyListeners();
+  }
+
+  void setAnimeInstance(EXtractAnimes instance) {
+    _animeInstance = instance;
+    notifyListeners();
+  }
+
+  void setNovelInstance(NovelSourceBase instance) {
+    _novelInstance = instance;
+    notifyListeners();
+  }
+
+  AnimeSourcehandler getAnimeInstace(){
+    return animehandler;
+  }
+
+  MangaSourcehandler getMangaInstance(){
+    return mangaSourcehandler;
+  }
+
+  NovelSourcehandler getNovelInstance(){
+    return novelSourcehandler;
+  }
+}
