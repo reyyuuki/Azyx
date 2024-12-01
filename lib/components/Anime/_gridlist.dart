c1a2ea2d4583e396cc0dc58cdee0b95624834a61
Rey Yuuki <irfan31331@gmail.com>
2024-12-01 16:15:08 +0530

    refactor: make full responsiveness to search gridlist  for anime

--- lib/components/Anime/_gridlist.dart
+++ lib/components/Anime/_gridlist.dart
@@ -1,36 +1,49 @@
+import 'dart:math';
 
 import 'package:daizy_tv/components/Anime/anime_item.dart';
 import 'package:flutter/material.dart';
 
 class GridList extends StatelessWidget {
-  const GridList({
-    super.key,
-    required this.data,
-    required this.route
-  });
-  final route;
+  final String route;
   final dynamic data;
+  const GridList({super.key, required this.data, required this.route});
 
   @override
   Widget build(BuildContext context) {
-    if(data == null){
+    if (data == null) {
       return const SizedBox.shrink();
     }
+
+    int itemCount = (MediaQuery.of(context).size.width ~/ 200).toInt();
+    int minCount = 3;
+    double gridWidth =
+        MediaQuery.of(context).size.width / max(itemCount, minCount);
+    double maxHeight = MediaQuery.of(context).size.height /
+        2.5; 
+    double gridHeight = min(gridWidth * 1.9, maxHeight);
     return Container(
       padding: const EdgeInsets.only(left: 12),
       color: Theme.of(context).colorScheme.surface,
-      child: GridView.builder(  
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, 
-          crossAxisSpacing: 5.0, 
-          childAspectRatio:
-              0.56, 
+      child: GridView.builder(
+        physics: const BouncingScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: max(itemCount, minCount),
+          crossAxisSpacing: 5.0,
+          childAspectRatio: gridWidth / gridHeight,
         ),
         itemCount: data!.length,
         itemBuilder: (context, index) {
           final item = data![index];
           final tagg = "${item['id']}List";
-          return ItemCard(id: item['id'].toString(), poster: item['poster'], type: item['type'], name: item['name'], rating: item['averageScore'], tagg: tagg, route: route,);
+          return ItemCard(
+            id: item['id'].toString(),
+            poster: item['poster'],
+            type: item['type'],
+            name: item['name'],
+            rating: item['averageScore'],
+            tagg: tagg,
+            route: route,
+          );
         },
       ),
     );
