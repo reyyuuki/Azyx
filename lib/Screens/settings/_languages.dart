a78fd72c276e77ccfcae75f9b1008e937d3b3672
Rey Yuuki <irfan31331@gmail.com>
2024-12-01 16:33:42 +0530

    feat: added downloads retries options and  make about page responsive

--- lib/Screens/settings/_languages.dart
+++ lib/Screens/settings/_languages.dart
@@ -1,13 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class Languages extends StatelessWidget {
-  const Languages ({super.key});
+  const Languages({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: const Center(child: Text("This page content seeing soon", style: TextStyle(fontSize: 20),)),
+      appBar: AppBar(
+        title: const Text(
+          "Languages",
+          style: TextStyle(fontFamily: "Poppins-Bold", fontSize: 20),
+        ),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            iconSize: Platform.isAndroid ? 25 : 35,
+            icon: const Icon(Icons.arrow_back_ios)),
+        centerTitle: true,
+      ),
+      body: const Center(
+          child: Text(
+        "This page content seeing soon",
+        style: TextStyle(fontSize: 20),
+      )),
     );
   }
 }
--- lib/Screens/settings/download_settings.dart
+++ lib/Screens/settings/download_settings.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:daizy_tv/components/Common/slider.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:iconsax/iconsax.dart';
 
 class DownloadSettings extends StatefulWidget {
   const DownloadSettings({super.key});
@@ -14,11 +15,13 @@ class DownloadSettings extends StatefulWidget {
 class _DownloadSettingsState extends State<DownloadSettings> {
   var box = Hive.box('app-data');
   late int parallelDownloads;
+  late int retries;
 
   @override
   void initState() {
     super.initState();
     parallelDownloads = box.get("parallelDownloads", defaultValue: 50);
+    retries = box.get("downloadRetries", defaultValue: 20);
   }
 
   @override
@@ -26,48 +29,57 @@ class _DownloadSettingsState extends State<DownloadSettings> {
     log(parallelDownloads.toString());
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
-        title: const Text(
-          "Download Settings",
-          style: TextStyle(fontFamily: "Poppins-Bold"),
-        ),
-        leading:  IconButton(
-          onPressed: (){
-            Navigator.pop(context);
-          },
-         icon: Icon(Icons.arrow_back_ios)),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: [
-            const SizedBox(
-              height: 40,
+      body: ListView(
+        padding: const EdgeInsets.all(8),
+        children: [
+          IconButton(
+              alignment: Alignment.topLeft,
+              hoverColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              iconSize: 30,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back_ios)),
+          const SizedBox(
+            height: 40,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              "Download settings",
+              style: TextStyle(fontFamily: "Poppins-Bold", fontSize: 30),
             ),
-            Row(
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Icon(
-                  Icons.speed,
-                  size: 30,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
                 Text(
                   "Download - Speed",
                   style: TextStyle(
                       fontFamily: "Poppins-Bold",
                       fontSize: 18,
                       color: Theme.of(context).colorScheme.primary),
-                )
+                ),
+                Icon(
+                  Icons.speed,
+                  size: 30,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
               ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Column(
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
               children: [
                 CustomSlider(
                     onChanged: (double value) {
@@ -117,9 +129,61 @@ class _DownloadSettingsState extends State<DownloadSettings> {
                   ],
                 ),
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Retries",
+                  style: TextStyle(
+                      fontFamily: "Poppins-Bold",
+                      fontSize: 18,
+                      color: Theme.of(context).colorScheme.primary),
+                ),
+                Icon(
+                  Iconsax.document_download,
+                  size: 30,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(Icons.downloading),
+                
+                Expanded(
+                  child: CustomSlider(
+                      onChanged: (double value) {
+                        setState(() {
+                          retries = value.toInt();
+                          box.put("downloadRetries", retries);
+                        });
+                      },
+                      divisions: 10,
+                      max: 100,
+                      min: 10,
+                      value: retries.toDouble()),
+                ),
+                
+                Text(
+                  retries.toString(),
+                  style: const TextStyle(
+                      fontFamily: "Poppins-Bold", fontSize: 16),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
