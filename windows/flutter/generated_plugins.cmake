34fe0ecbe651edff9f5c42c5d4b907c49229573f
Rey Yuuki <irfan31331@gmail.com>
2024-12-28 15:15:20 +0530

    build: update packages

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,10 +5,13 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_webview_window
   dynamic_color
+  flutter_qjs
+  isar_flutter_libs
   media_kit_libs_windows_video
   media_kit_video
   permission_handler_windows
   screen_brightness_windows
+  share_plus
   url_launcher_windows
   window_to_front
 )
